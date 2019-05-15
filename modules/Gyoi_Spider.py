65249cbc8155d9bf51d67d7cab0a03128355d499
GyoiThon <gyoiler3@gmail.com>
2019-05-15 10:58:19 +0900

    Fix bugs.

--- modules/Gyoi_Spider.py
+++ modules/Gyoi_Spider.py
@@ -20,6 +20,17 @@ class SimpleSpider(scrapy.Spider):
         self.proxy_server = getattr(self, 'proxy_server', None)
         self.user_agent = getattr(self, 'user_agent', None)
         self.encoding = getattr(self, 'encoding', None)
+
+        # HTTP headers.
+        self.http_req_header = {'User-Agent': self.user_agent,
+                                'Connection': 'keep-alive',
+                                'Accept-Language': 'ja,en-US;q=0.7,en;q=0.3',
+                                'Accept-Encoding': 'gzip, deflate',
+                                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                                'Upgrade-Insecure-Requests': '1',
+                                'Content-Type': 'application/x-www-form-urlencoded',
+                                'Cache-Control': 'no-cache'}
+
         self.custom_settings = {
             'CONCURRENT_REQUESTS': self.concurrent,
             'CONCURRENT_REQUESTS_PER_DOMAIN': self.concurrent,
@@ -40,11 +51,9 @@ class SimpleSpider(scrapy.Spider):
         # Set proxy server.
         if self.proxy_server != '':
             proxy = {'proxy': self.proxy_server}
-            yield Request(url, self.parse, meta=proxy)
+            yield Request(url, self.parse, meta=proxy, headers=self.http_req_header)
         else:
-            yield Request(url, self.parse)
-
-        # yield Request(url, self.parse)
+            yield Request(url, self.parse, headers=self.http_req_header)
 
     def parse(self, response):
         self.fout.write(response.body.decode(self.encoding))
@@ -55,9 +64,9 @@ class SimpleSpider(scrapy.Spider):
             # Set proxy server.
             if self.proxy_server != '':
                 proxy = {'proxy': self.proxy_server}
-                yield scrapy.Request(full_url, callback=self.parse_item, meta=proxy)
+                yield scrapy.Request(full_url, callback=self.parse_item, meta=proxy, headers=self.http_req_header)
             else:
-                yield scrapy.Request(full_url, callback=self.parse_item)
+                yield scrapy.Request(full_url, callback=self.parse_item, headers=self.http_req_header)
 
     def parse_item(self, response):
         urls = []
