78bd3a167cda5f67070b152872e5a55f528ecef2
fargus <fargusplumdoodle@gmail.com>
2019-06-19 08:01:15 -0700

    updated run file

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,4 @@
-docker stop sst_api_gyoithon 2> /dev/null
-docker rm sst_api_gyoithon  2> /dev/null
+docker rm -f gyoithon  2> /dev/null
 
 docker run \
 	--rm \
@@ -7,4 +6,4 @@ docker run \
 	-v ${PWD}/host.txt:/opt/GyoiThon/host.txt \
 	-v ${PWD}/report:/opt/GyoiThon/report \
 	--name gyoithon_test \
-	sst_api_gyoithon 
+	gyoithon 
